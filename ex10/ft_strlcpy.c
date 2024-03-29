d4419c555404c5997f00b1315a0fbae60d3b6caf
Alejandro Hernandez Hernandez <alejhern@cbr12s5.42barcelona.com>
2024-03-29 01:36:40 +0100

    Ejercicio 10

---
+++ ex10/ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/28 17:15:23 by alejhern          #+#    #+#             */
+/*   Updated: 2024/03/29 01:36:12 by alejhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	c;
+	unsigned int	i;
+
+	c = 0;
+	i = 0;
+	while (src[c])
+		c++;
+	if (size != 0)
+	{
+		while (src[i] && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (c);
+}
