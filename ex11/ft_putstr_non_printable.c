04f87248d5a20826f538507b6f844c580bb5848d
Alejandro Hernandez Hernandez <alejhern@cbr15s2.42barcelona.com>
2024-03-30 01:10:59 +0100

    paco pasa EX11

--- ex11/ft_putstr_non_printable.c
+++ ex11/ft_putstr_non_printable.c
@@ -6,35 +6,32 @@
 /*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 01:08:55 by alejhern          #+#    #+#             */
-/*   Updated: 2024/03/29 02:34:38 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/03/30 01:08:10 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void	ft_putstr_non_printable(char *str)
 {
 	int		i;
-	char	aux[4096];
 
 	i = 0;
 	while (str[i])
 	{
 		if (str[i] < 32 || str[i] > 126)
 		{
-			aux[i] = '\\';
-			aux[i + 1] = "0123456789abcdef"[str[i] >> 4];
-			aux[i + 2] = "0123456789abcdef"[str[i] & 0x0F];
-			i += 2;
+			ft_putchar('\\');
+			ft_putchar("0123456789abcdef"[(unsigned char)str[i] / 16]);
+			ft_putchar("0123456789abcdef"[(unsigned char)str[i] % 16]);
 		}
 		else
-			aux[i] = str[i];
+			ft_putchar(str[i]);
 		i++;
 	}
-	aux[i] = '\0';
-	i = 0;
-	while (aux[i])
-	{
-		str[i] = aux[i];
-		i++;
-	}
-	str[i] = '\0';
 }
